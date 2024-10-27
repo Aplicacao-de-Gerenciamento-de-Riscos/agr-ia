b91ea1f843a275f13090d5068f148092bd4eec7d
Brenda Aldrovandi Gaio <68788724+brendagaioo@users.noreply.github.com>
2024-10-27 20:18:41 -0300

    feat: add code performance improvements

--- crud.py
+++ crud.py
@@ -1,73 +1,63 @@
-# crud.py
 from sqlalchemy.orm import Session
 from models import Issue, Sprint, VersionIssue, Version, Project
 import pandas as pd
 
 def load_data(db: Session):
-    project_sprint_map = {}  # Mapeamento de projetos para sprints
-    #O mapeamento de projetos para sprints é feito utilizando um dicionário, onde analisamos o nome das sprints e verificamos se a key do projeto está presente no nome da sprint
-    #Se estiver presente, adicionamos a sprint ao projeto correspondente
-    # Carregar todas as sprints e seus projetos
-    sprints = db.query(Sprint).all()
-    projects = db.query(Project).all()
-    for sprint in sprints:
-        for project in projects:
-            if project.key in sprint.name:
-                if project.cod_project not in project_sprint_map:
-                    project_sprint_map[project.cod_project] = []
-                project_sprint_map[project.cod_project].append(sprint)
+    # Carregar sprints e projetos com uma única consulta
+    projects_with_sprints = (db.query(Project.cod_project, Project.key, Sprint.cod_sprint, Sprint.name)
+                                .join(Sprint, Sprint.name.contains(Project.key))
+                                .all())
 
-    # Printar o mapeamento de projetos para sprints
-    # for project in project_sprint_map:
-    #     print(f"Project {project}:")
-    #     for sprint in project_sprint_map[project]:
-    #         print(f"  - Sprint {sprint.cod_sprint}: {sprint.name}")
+    # Criar o mapeamento de projetos para sprints
+    project_sprint_map = {}
+    for project_cod, project_key, sprint_cod, sprint_name in projects_with_sprints:
+        if project_cod not in project_sprint_map:
+            project_sprint_map[project_cod] = []
+        project_sprint_map[project_cod].append(sprint_cod)
 
-    # Carregar todas as issues e suas relações com sprints, versões e projetos
-    issues = (db.query(Issue)
-                .join(Sprint, Issue.cod_sprint == Sprint.cod_sprint)  # Relaciona Issues com Sprints
-                .join(VersionIssue, Issue.cod_issue == VersionIssue.cod_issue)  # Relaciona Issues com VersionIssues
-                .join(Version, VersionIssue.cod_version == Version.cod_version)  # Relaciona VersionIssues com Versions
-                .join(Project, Version.cod_project == Project.cod_project)  # Relaciona Versions com Project
+    # Carregar todas as issues e relações em uma única consulta
+    issues = (db.query(Issue.cod_issue, Issue.time_original_estimate, Issue.timespent, Issue.priority, Issue.cod_sprint,
+                       Issue.cod_epic, Issue.issuetype, VersionIssue.cod_version, Project.cod_project)
+                .join(Sprint, Issue.cod_sprint == Sprint.cod_sprint)
+                .join(VersionIssue, Issue.cod_issue == VersionIssue.cod_issue)
+                .join(Version, VersionIssue.cod_version == Version.cod_version)
+                .join(Project, Version.cod_project == Project.cod_project)
                 .all())
 
-    # Converte as issues para um DataFrame do pandas
+    # Converter as issues para um DataFrame
     issue_data = pd.DataFrame([{
         'cod_issue': issue.cod_issue,
         'time_original_estimate': issue.time_original_estimate,
         'timespent': issue.timespent,
         'priority': issue.priority,
-        # 'status': issue.status,
-        'cod_sprint': issue.cod_sprint,  # Sprint associada
-        'cod_epic': issue.cod_epic,  # Épico associado
-        'issuetype': issue.issuetype,  # Tipo de Issue
-        #Setar o cod_project para o projeto correspondente à sprint
-        'cod_project': next((project.cod_project for project in projects if any(sprint.cod_sprint == issue.cod_sprint for sprint in project_sprint_map[project.cod_project])), None),
-        'cod_version': issue.versions[0].cod_version  # Versão associada
+        'cod_sprint': issue.cod_sprint,
+        'cod_epic': issue.cod_epic,
+        'issuetype': issue.issuetype,
+        'cod_project': issue.cod_project,
+        'cod_version': issue.cod_version
     } for issue in issues])
 
-    # Fazer as transformações necessárias (one-hot encoding para status, priority, issuetype e cod_epic)
+    # Verifique a existência do mapeamento de projeto para sprint antes de atribuir
+    issue_data['cod_project'] = issue_data['cod_sprint'].map(
+        lambda sprint: next((project for project, sprints in project_sprint_map.items() if sprint in sprints), None)
+    )
+
+    # Aplicar one-hot encoding para colunas categóricas
     issue_data = pd.get_dummies(issue_data, columns=['priority', 'issuetype', 'cod_epic'])
 
-    # Agrupar por versão e projeto (cod_version, cod_project)
-    version_data = issue_data.groupby(['cod_version', 'cod_project']).agg({
-        'time_original_estimate': 'sum',  # Soma o tempo estimado para todas as issues da versão
-        'timespent': 'sum',  # Soma o tempo gasto em todas as issues da versão
-        # Para one-hot encoding, usamos a média (se houver pelo menos 1 ocorrência, será > 0)
+    # Agrupar por versão e projeto
+    aggregation_functions = {
+        'time_original_estimate': 'sum',
+        'timespent': 'sum',
         **{col: 'mean' for col in issue_data.columns if col.startswith('priority_')},
-        **{col: 'mean' for col in issue_data.columns if col.startswith('status_')},
         **{col: 'mean' for col in issue_data.columns if col.startswith('issuetype_')},
-        **{col: 'sum' for col in issue_data.columns if col.startswith('cod_epic_')}  # Soma ao invés de média para cod_epic
-    }).reset_index()
+        **{col: 'sum' for col in issue_data.columns if col.startswith('cod_epic_')}
+    }
+    version_data = issue_data.groupby(['cod_version', 'cod_project']).agg(aggregation_functions).reset_index()
 
-    # Remover coluna de épico que só possuem valores em uma linha
-    # Buscar colunas de épico
+    # Remover colunas de épico com poucos valores > 0
     epic_cols = [col for col in version_data.columns if col.startswith('cod_epic_')]
-
-    # Iterar sobre as colunas de épico e verificar em quantas linhas o valor é > 0
-    for col in epic_cols:
-        if version_data[col].gt(0).sum() < 2:
-            # Remover a coluna caso só tenha 1 ou 0 ocorrências
-            version_data.drop(columns=[col], inplace=True)
+    cols_to_drop = [col for col in epic_cols if version_data[col].gt(0).sum() < 2]
+    version_data.drop(columns=cols_to_drop, inplace=True)
 
     return version_data
--- main.py
+++ main.py
@@ -1,24 +1,55 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import get_db  # Importar função para obter sessão do banco de dados
-from crud import load_data  # Sua função de carregamento de dados
-from predict_model import predict_delay  # Função de previsão do modelo treinado
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import numpy as np
+from crud import load_data
+from database import get_db
+import joblib
 
 app = FastAPI()
 
-@app.get("/predict_delay/{cod_version}")
-def predict(cod_version: int, db: Session = Depends(get_db)):
-    # Passo 1: Carregar dados
+
+# Função para carregar o modelo treinado
+def load_trained_model():
+    return joblib.load("tuned_xgboost_model.joblib")
+
+
+# Função para prever o risco de atraso de uma versão específica
+def predict_delay(version_data):
+    model = load_trained_model()
+
+    # Obtenha a probabilidade de atraso
+    delay_risk_percentage = float(model.predict_proba(version_data)[0][1]) * 100  # Converte para float
+
+    return delay_risk_percentage
+
+
+@app.get("/predict-delay/{version_id}")
+def get_predict_delay(version_id: int):
+    db = next(get_db())
+
+    # Carregar os dados da versão específica em formato de DataFrame
     version_data = load_data(db)
+    version_data = version_data[version_data['cod_version'] == version_id].drop(columns=['cod_version', 'cod_project'])
 
-    # Passo 2: Filtrar dados para a versão especificada
-    version_info = version_data[version_data['cod_version'] == cod_version]
+    if version_data.empty:
+        raise HTTPException(status_code=404, detail="Versão não encontrada")
 
-    # Passo 3: Executar previsão do modelo
-    delay_risk = predict_delay(version_info)
+    # Fazer a predição e obter o risco e épicos que mais impactam
+    delay_risk_percentage = predict_delay(version_data)
 
-    # Passo 4: Retornar resposta
     return {
-        "version_id": cod_version,
-        "delay_risk_percentage": delay_risk
+        "delay_risk_percentage": delay_risk_percentage
     }
+
+
+# Exemplo de uso
+if __name__ == "__main__":
+    db = next(get_db())
+    # Carregar os dados da versão específica em formato de DataFrame
+    version_data = load_data(db)
+    version_data = version_data[version_data['cod_version'] == 16406].drop(columns=['cod_version', 'cod_project'])
+
+    # Fazer a predição e obter o risco e épicos que mais impactam
+    delay_risk_percentage = predict_delay(version_data)
+
+    print("Porcentagem de Risco de Atraso:", delay_risk_percentage)
