15f3c52e06dc8f599568d9446d483f7735f87b86
Brenda Aldrovandi Gaio <68788724+brendagaioo@users.noreply.github.com>
2024-11-07 03:01:49 -0300

    feat: change predict delay endpoint to accept a list of version ids

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import numpy as np
@@ -23,23 +25,30 @@ def predict_delay(version_data):
     return delay_risk_percentage
 
 
-@app.get("/predict-delay/{version_id}")
-def get_predict_delay(version_id: int):
+@app.get("/predict-delay")
+def get_predict_delay(version_ids: str):
+    # Converter a string 'id1,id2,id3' em uma lista de inteiros
+    try:
+        version_id_list = [int(id) for id in version_ids.split(',')]
+    except ValueError:
+        raise HTTPException(status_code=400, detail="IDs devem ser números inteiros separados por vírgulas")
+
     db = next(get_db())
 
-    # Carregar os dados da versão específica em formato de DataFrame
-    version_data = load_data(db)
-    version_data = version_data[version_data['cod_version'] == version_id].drop(columns=['cod_version', 'cod_project'])
+    delay_risk_percentage = []
 
-    if version_data.empty:
-        raise HTTPException(status_code=404, detail="Versão não encontrada")
+    for version_id in version_id_list:
+        # Carregar os dados da versão específica em formato de DataFrame
+        version_data = load_data(db)
+        version_data = version_data[version_data['cod_version'] == version_id].drop(columns=['cod_version', 'cod_project'])
 
-    # Fazer a predição e obter o risco e épicos que mais impactam
-    delay_risk_percentage = predict_delay(version_data)
+        # Fazer a predição e obter o risco e épicos que mais impactam
+        delay_risk_percentage.append({
+            "version_id": version_id,
+            "delay_risk_percentage": predict_delay(version_data)
+        })
 
-    return {
-        "delay_risk_percentage": delay_risk_percentage
-    }
+    return delay_risk_percentage
 
 
 # Exemplo de uso
